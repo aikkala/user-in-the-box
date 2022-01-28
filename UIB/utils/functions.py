db20520fe803eef954c6cacfd9b5ab830059a2d5
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2022-01-28 15:02:30 +0200

    Add a bunch of helper functions

---
+++ UIB/utils/functions.py
@@ -0,0 +1,20 @@
+import pathlib
+import os
+from datetime import datetime
+import sys
+import select
+
+def project_path():
+  return pathlib.Path(__file__).parent.parent.absolute()
+
+def output_path():
+  return os.path.join(project_path().parent.absolute(), "output")
+
+def strtime():
+  return datetime.utcfromtimestamp(datetime.now().timestamp()).strftime('%Y-%m-%dT%H-%M-%SZ')
+
+def timeout_input(prompt, timeout=30, default=""):
+  print(prompt, end='\n>> ', flush=True)
+  inputs, outputs, errors = select.select([sys.stdin], [], [], timeout)
+  print()
+  return sys.stdin.readline().strip() if inputs else default
