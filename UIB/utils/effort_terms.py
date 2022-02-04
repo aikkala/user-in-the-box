2f45f67013157072b93711d0eeda178a89978c96
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2022-02-04 20:00:20 +0200

    Use qfrc_inverse instead of qfrc_unc

--- UIB/utils/effort_terms.py
+++ UIB/utils/effort_terms.py
@@ -1,5 +1,6 @@
 import numpy as np
 from abc import ABC, abstractmethod
+import mujoco_py
 
 class BaseTerm(ABC):
   @abstractmethod
@@ -18,11 +19,12 @@ class Neural(BaseTerm):
     return "Neural"
 
 class Composite(BaseTerm):
-  def __init__(self, weight=1e-7):
+  def __init__(self, weight=1e-5):
     self.weight = weight
   def get(self, env):
+    mujoco_py.cymj._mj_inverse(env.sim.model, env.sim.data)
     angle_acceleration = np.sum(env.sim.data.qacc[env.independent_joints] ** 2)
-    energy = np.sum(env.sim.data.qvel[env.independent_joints] ** 2 * env.sim.data.qfrc_unc[env.independent_joints] ** 2)
+    energy = np.sum(env.sim.data.qvel[env.independent_joints] ** 2 * env.sim.data.qfrc_inverse[env.independent_joints] ** 2)
     return self.weight * (energy + 0.05 * angle_acceleration)
   def __repr__(self):
     return "Composite"
