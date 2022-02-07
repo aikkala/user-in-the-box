c0e2d8743ca1aee80cdc470580077346151700fd
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2022-02-07 11:30:47 +0200

    Save config of the run

--- UIB/train/trainer.py
+++ UIB/train/trainer.py
@@ -19,15 +19,20 @@ if __name__=="__main__":
     # Ask user to name this run
     name = timeout_input("Give a name for this run. Input empty string or wait for 30 seconds for a random name.",
                          timeout=30, default="")
+    config["name"] = name
 
-  # Initialsie wandb
+  # Initialise wandb
   run = wandb.init(project="uib", name=name, config=config, sync_tensorboard=True, save_code=True, dir=output_path())
 
   # Define output directories
   model_folder = os.path.join(output_path(), config["env_name"], 'trained-models')
 
   # Initialise model
-  model = config["model"](config, run_folder=os.path.join(model_folder, run.name))
+  run_folder = os.path.join(model_folder, run.name)
+  model = config["model"](config, run_folder=run_folder)
+
+  # Save config
+  np.save(os.path.join(run_folder, 'config.npy'), config)
 
   # Haven't figured out how to periodically save models in wandb; this is currently done inside the model class
   # TODO this doesn't seem to work; do the files need to be in wandb.run.dir?
