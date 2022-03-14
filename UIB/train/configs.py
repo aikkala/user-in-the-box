01a05508712464670fdceb7b5c76181b92e38b31
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2022-03-14 18:44:18 +0200

    Update configs

--- UIB/train/configs.py
+++ UIB/train/configs.py
@@ -12,11 +12,11 @@ from UIB.utils import effort_terms
 import UIB.envs.mobl_arms.pointing.reward_functions as pointing_rewards
 import UIB.envs.mobl_arms.iso_pointing.reward_functions as iso_pointing_rewards
 import UIB.envs.mobl_arms.tracking.reward_functions as tracking_rewards
-import UIB.envs.mobl_arms.choosing.reward_functions as choosing_rewards
-
+import UIB.envs.mobl_arms.button_press.reward_functions as button_press_rewards
+import UIB.envs.mobl_arms.remote_driving.reward_functions as driving_rewards
 
 mobl_arms_pointing_v0 = {
-  "name": "pointing-v0-florian",
+  "name": "pointing-v0-old-model-new-muscles-3CC-r",
   "model": PPO,
   "total_timesteps": 100_000_000,
   "env_name": "UIB:mobl-arms-pointing-v0",
@@ -25,9 +25,8 @@ mobl_arms_pointing_v0 = {
   "device": "cpu",
   "env_kwargs": {"target_radius_limit": np.array([0.05, 0.15]),
                  "action_sample_freq": 20,
-                 "effort_term": effort_terms.Neural(),
-                 "reward_function": pointing_rewards.NegativeExpDistanceWithHitBonus(k=10),
-                 "callbacks": []},
+                 "effort_term": effort_terms.CumulativeFatigue(),
+                 "reward_function": pointing_rewards.NegativeExpDistanceWithHitBonus(k=10)},
   "policy_type": ActorCriticPolicyTanhActions,
   "policy_kwargs": {"activation_fn": torch.nn.LeakyReLU,
                     "net_arch": [256, 256],
@@ -37,7 +36,7 @@ mobl_arms_pointing_v0 = {
 }
 
 mobl_arms_pointing_v1 = {
-  "name": "pointing-v1-florian",
+  "name": "pointing-v1-patch-v1",
   "model": PPO,
   "total_timesteps": 100_000_000,
   "env_name": "UIB:mobl-arms-pointing-v1",
@@ -46,6 +45,7 @@ mobl_arms_pointing_v1 = {
   "device": "cuda",
   "env_kwargs": {"target_radius_limit": np.array([0.05, 0.15]),
                  "action_sample_freq": 20,
+                 "shoulder_variant": "patch-v1",
                  "effort_term": effort_terms.Neural(),
                  "reward_function": pointing_rewards.NegativeExpDistanceWithHitBonus(k=10),
                  "callbacks": []},
@@ -87,17 +87,17 @@ mobl_arms_tracking_v1 = {
 }
 
 
-mobl_arms_choosing_v1 = {
-  "name": "test-choosing",
+mobl_arms_button_press_v1 = {
+  "name": "test-button-press",
   "model": PPO,
   "total_timesteps": 100_000_000,
-  "env_name": "UIB:mobl-arms-choosing-v1",
+  "env_name": "UIB:mobl-arms-button-press-v1",
   "start_method": 'spawn' if 'Microsoft' in uname().release else 'forkserver',
   "num_workers": 10,
   "device": "cuda",
   "env_kwargs": {"action_sample_freq": 20,
                  "effort_term": effort_terms.Neural(),
-                 "reward_function": choosing_rewards.NegativeExpDistanceWithHitBonus(),
+                 "reward_function": button_press_rewards.NegativeExpDistanceWithHitBonus(),
                  "callbacks": []},
   "policy_type": MultiInputActorCriticPolicyTanhActions,
   "policy_kwargs": {"activation_fn": torch.nn.LeakyReLU,
@@ -110,7 +110,7 @@ mobl_arms_choosing_v1 = {
 }
 
 mobl_arms_iso_pointing_v1 = {
-  "name": "iso-pointing-U1",
+  "name": "iso-pointing-U1-patch-v1-dwell",
   "model": PPO,
   "total_timesteps": 100_000_000,
   "env_name": "UIB:mobl-arms-iso-pointing-v1",
@@ -118,7 +118,9 @@ mobl_arms_iso_pointing_v1 = {
   "num_workers": 10,
   "device": "cuda",
   "env_kwargs": {"user": "U1",
-                 "target_radius_limit": np.array([0.025, 0.15]),
+                 "evaluate": True,
+                 "shoulder_variant": "patch-v1",
+                 "target_radius_limit": np.array([0.05, 0.05]),
                  "action_sample_freq": 20,
                  "effort_term": effort_terms.Neural(),
                  "reward_function": iso_pointing_rewards.NegativeExpDistanceWithHitBonus(k=10),
@@ -134,15 +136,102 @@ mobl_arms_iso_pointing_v1 = {
 }
 
 mobl_arms_remote_driving_v1 = {
-  "name": "driving",
+  "name": "driving-updated-no-early-termination",
   "model": PPO,
   "total_timesteps": 100_000_000,
-  "env_name": "UIB:mobl-arms-remote_driving-v1",
+  "env_name": "UIB:mobl-arms-remote-driving-v1",
+  "start_method": 'spawn' if 'Microsoft' in uname().release else 'forkserver',
+  "num_workers": 10,
+  "device": "cuda",
+  "env_kwargs": {"direction": "horizontal",
+                 "action_sample_freq": 20,
+                 "effort_term": effort_terms.Neural(),
+                 "episode_length_seconds_extratime": 0,
+                 "episode_length_seconds": 10,
+                 "car_velocity_threshold": 0.0,
+                 "reward_function_joystick": driving_rewards.NegativeExpDistance(shift=-1, scale=1),
+                 "reward_function_target": driving_rewards.NegativeExpDistance(shift=-1, scale=0.1),
+                 "reward_function_joystick_bonus": driving_rewards.RewardBonus(bonus=0.8, onetime=True),
+                 "reward_function_target_bonus": driving_rewards.RewardBonus(bonus=8, onetime=False)
+  },
+  "policy_type": MultiInputActorCriticPolicyTanhActions,
+  "policy_kwargs": {"activation_fn": torch.nn.LeakyReLU,
+                    "net_arch": [256, 256],
+                    "log_std_init": 0.0,
+                    "features_extractor_class": VisualAndProprioceptionExtractor,
+                    "normalize_images": False},
+  "lr": linear_schedule(initial_value=5e-5, min_value=1e-7, threshold=0.8),
+  "nsteps": 4000, "batch_size": 500, "target_kl": 1.0, "save_freq": 5000000
+}
+
+
+
+#### Below are the configs used to train the models for the UIST paper ####
+
+mobl_arms_pointing_uist = {
+  "name": "pointing-v1-patch-v1",
+  "model": PPO,
+  "total_timesteps": 100_000_000,
+  "env_name": "UIB:mobl-arms-pointing-v1",
+  "start_method": 'spawn' if 'Microsoft' in uname().release else 'forkserver',
+  "num_workers": 10,
+  "device": "cuda",
+  "env_kwargs": {"target_radius_limit": np.array([0.05, 0.15]),
+                 "action_sample_freq": 20,
+                 "shoulder_variant": "patch-v1",
+                 "effort_term": effort_terms.Neural(),
+                 "reward_function": pointing_rewards.NegativeExpDistanceWithHitBonus(k=10),
+                 "callbacks": []},
+  "policy_type": MultiInputActorCriticPolicyTanhActions,
+  "policy_kwargs": {"activation_fn": torch.nn.LeakyReLU,
+                    "net_arch": [256, 256],
+                    "log_std_init": 0.0,
+                    "features_extractor_class": VisualAndProprioceptionExtractor,
+                    "normalize_images": False},
+  "lr": linear_schedule(initial_value=5e-5, min_value=1e-7, threshold=0.8),
+  "nsteps": 4000, "batch_size": 500, "target_kl": 1.0, "save_freq": 5000000
+}
+
+target_speed_curriculum = LinearCurriculum("target_speed_curriculum", start_value=0, end_value=1,
+                                           end_timestep=60_000_000, start_timestep=40_000_000)
+mobl_arms_tracking_uist = {
+  "name": "tracking-v1-patch-v1",
+  "model": PPO,
+  "total_timesteps": 100_000_000,
+  "env_name": "UIB:mobl-arms-tracking-v1",
+  "start_method": 'spawn' if 'Microsoft' in uname().release else 'forkserver',
+  "num_workers": 10,
+  "device": "cuda",
+  "env_kwargs": {"target_radius": 0.05,
+                 "action_sample_freq": 20,
+                 "shoulder_variant": "patch-v1",
+                 "effort_term": effort_terms.Neural(),
+                 "reward_function": tracking_rewards.NegativeDistance(),
+                 "freq_curriculum": target_speed_curriculum.value,
+                 "episode_length_seconds": 10,
+                 "callbacks": [target_speed_curriculum]},
+  "policy_type": MultiInputActorCriticPolicyTanhActions,
+  "policy_kwargs": {"activation_fn": torch.nn.LeakyReLU,
+                    "net_arch": [256, 256],
+                    "log_std_init": 0.0,
+                    "features_extractor_class": VisualAndProprioceptionExtractor,
+                    "normalize_images": False},
+  "lr": linear_schedule(initial_value=5e-5, min_value=1e-7, threshold=0.8),
+  "nsteps": 4000, "batch_size": 500, "target_kl": 1.0, "save_freq": 5000000
+}
+
+mobl_arms_button_press_uist = {
+  "name": "button-press-v1-patch-v1",
+  "model": PPO,
+  "total_timesteps": 100_000_000,
+  "env_name": "UIB:mobl-arms-button-press-v1",
   "start_method": 'spawn' if 'Microsoft' in uname().release else 'forkserver',
   "num_workers": 10,
   "device": "cuda",
   "env_kwargs": {"action_sample_freq": 20,
-                 "effort_term": effort_terms.Neural()},
+                 "shoulder_variant": "patch-v1",
+                 "effort_term": effort_terms.Neural(),
+                 "reward_function": button_press_rewards.NegativeExpDistanceWithHitBonus()},
   "policy_type": MultiInputActorCriticPolicyTanhActions,
   "policy_kwargs": {"activation_fn": torch.nn.LeakyReLU,
                     "net_arch": [256, 256],
