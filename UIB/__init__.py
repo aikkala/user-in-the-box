cf7b12d63daf2670819f9f249552aa3cd24bc3be
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2022-03-14 13:24:49 +0200

    Fix a typo

--- UIB/__init__.py
+++ UIB/__init__.py
@@ -4,6 +4,6 @@ register(id='mobl-arms-pointing-v0', entry_point='UIB.envs.mobl_arms.pointing.Po
 register(id='mobl-arms-pointing-v1', entry_point='UIB.envs.mobl_arms.pointing.PointingEnv:ProprioceptionAndVisual')
 register(id='mobl-arms-iso-pointing-v1', entry_point='UIB.envs.mobl_arms.iso_pointing.ISOPointingEnv:ProprioceptionAndVisual')
 register(id='mobl-arms-tracking-v1', entry_point='UIB.envs.mobl_arms.tracking.TrackingEnv:ProprioceptionAndVisual')
-register(id='mobl-arms-button_press-v1', entry_point='UIB.envs.mobl_arms.button_press.ButtonPressEnv:ProprioceptionAndVisual')
+register(id='mobl-arms-button-press-v1', entry_point='UIB.envs.mobl_arms.button_press.ButtonPressEnv:ProprioceptionAndVisual')
 register(id='mobl-arms-remote-driving-v1', entry_point='UIB.envs.mobl_arms.remote_driving.RemoteDrivingEnv:ProprioceptionAndVisual')
 
