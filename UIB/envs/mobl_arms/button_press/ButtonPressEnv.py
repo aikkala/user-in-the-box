f284c404b0f66a5bbe37f32bfd4a687c0a08845e
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2022-03-15 16:45:13 +0200

    Use smaller buttons

--- UIB/envs/mobl_arms/button_press/ButtonPressEnv.py
+++ UIB/envs/mobl_arms/button_press/ButtonPressEnv.py
@@ -65,9 +65,9 @@ class ButtonPressEnv(FixedEye):
 
     # Add button
     button = ET.Element('body', name=f'button{idx}-body', pos=np.array2string(position)[1:-1], euler=euler)
-    button.append(ET.Element('geom', name=f'button{idx}', type="box", size="0.05 0.05 0.01",
+    button.append(ET.Element('geom', name=f'button{idx}', type="box", size="0.025 0.025 0.01",
                              rgba=np.array2string(color)[1:-1]))
-    button.append(ET.Element('site', name=f'button{idx}-site', pos="0 0 0.01", type="box", size="0.045 0.045 0.01",
+    button.append(ET.Element('site', name=f'button{idx}-site', pos="0 0 0.01", type="box", size="0.02 0.02 0.01",
                              rgba="0.5 0.5 0.5 0.0"))
     worldbody.append(button)
 
