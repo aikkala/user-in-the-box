e49f65cacef85c14813576e596a401859c1d67c6
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2022-05-12 13:30:12 +0300

    Remove old feature extractor class, remove commented lines

--- uitb/rl/sb3/feature_extractor.py
+++ uitb/rl/sb3/feature_extractor.py
@@ -5,66 +5,6 @@ from torch import nn
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
 
-class VisualAndProprioceptionExtractor(BaseFeaturesExtractor):
-  def __init__(self, observation_space: gym.spaces.Dict):
-    # We do not know features-dim here before going over all the items,
-    # so put something dummy for now. PyTorch requires calling
-    # nn.Module.__init__ before adding modules
-    super(VisualAndProprioceptionExtractor, self).__init__(observation_space, features_dim=1)
-
-    extractors = {}
-
-    total_concat_size = 0
-    # We need to know size of the output of this extractor,
-    # so go over all the spaces and compute output feature sizes
-    for key, subspace in observation_space.spaces.items():
-      if key == "vision":
-        fake_observation = th.as_tensor(observation_space["vision"].sample()[None])
-        # Run through a simple CNN
-        cnn = nn.Sequential(
-          nn.Conv2d(in_channels=fake_observation.shape[1], out_channels=8, kernel_size=(3,3), padding=(1,1), stride=(2,2)),
-          nn.LeakyReLU(),
-          nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), padding=(1,1), stride=(2,2)),
-          nn.LeakyReLU(),
-          nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3,3), padding=(1,1), stride=(2,2)),
-          nn.LeakyReLU(),
-          nn.Flatten())
-
-        # Compute shape by doing one forward pass
-        with th.no_grad():
-            n_flatten = cnn(fake_observation).shape[1]
-
-        extractors[key] = nn.Sequential(
-          cnn,
-          nn.Linear(in_features=n_flatten, out_features=256),
-          nn.LeakyReLU())
-        #total_concat_size += subspace.shape[1] * subspace.shape[2] * 1
-        total_concat_size += 256
-      elif key == "proprioception":
-        # Run through a simple MLP
-        extractors[key] = nn.Sequential(
-          nn.Linear(subspace.shape[0], 128),
-          nn.LeakyReLU())
-        total_concat_size += 128
-      elif key == "ocular":
-        # Do nothing with this for now
-        pass
-
-    self.extractors = nn.ModuleDict(extractors)
-
-    # Update the features dim manually
-    self._features_dim = total_concat_size
-
-  def forward(self, observations) -> th.Tensor:
-    encoded_tensor_list = []
-
-    # self.extractors contain nn.Modules that do all the processing.
-    for key, extractor in self.extractors.items():
-      encoded_tensor_list.append(extractor(observations[key]))
-    # Return a (B, self._features_dim) PyTorch tensor, where B is batch dimension.
-    return th.cat(encoded_tensor_list, dim=1)
-
-
 class FeatureExtractor(BaseFeaturesExtractor):
   def __init__(self, observation_space: gym.spaces.Dict, extractors):
     # We do not know features-dim here before going over all the items,
@@ -79,8 +19,7 @@ class FeatureExtractor(BaseFeaturesExtractor):
     for key, extractor in extractors.items():
       if extractor is None:
         extractors[key] = nn.Identity()
-    #  total_concat_size += extractors[key](th.from_numpy(fake_observation[key])[None]).shape[1]
-    total_concat_size = 256 + 128
+      total_concat_size += extractors[key](th.from_numpy(fake_observation[key])[None]).shape[1]
 
     # Convert into ModuleDict
     self.extractors = nn.ModuleDict(extractors)
@@ -90,12 +29,8 @@ class FeatureExtractor(BaseFeaturesExtractor):
 
   def forward(self, observations) -> th.Tensor:
     encoded_tensor_list = []
-
     # self.extractors contain nn.Modules that do all the processing.
-    #for key, extractor in self.extractors.items():
-    #  encoded_tensor_list.append(extractor(observations[key]))
-    encoded_tensor_list.append(self.extractors["vision"](observations["vision"]))
-    combined = th.cat([observations["proprioception"], observations["stateful_information"]] ,dim=1)
-    encoded_tensor_list.append(self.extractors["proprioception"](combined))
+    for key, extractor in self.extractors.items():
+      encoded_tensor_list.append(extractor(observations[key]))
     # Return a (B, self._features_dim) PyTorch tensor, where B is batch dimension.
     return th.cat(encoded_tensor_list, dim=1)
