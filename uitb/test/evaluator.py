667ee6ff0792a0f77667a520e47b483a81b16b12
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2022-05-12 14:15:38 +0300

    WIP: initialisation of environment might still change, collection of statistics is missing from Simulator class

--- uitb/test/evaluator.py
+++ uitb/test/evaluator.py
@@ -6,14 +6,14 @@ from stable_baselines3 import PPO
 import re
 import argparse
 import scipy.ndimage
-import pickle
+import dill
 from pathlib import Path
 from collections import defaultdict
 import matplotlib.pyplot as pp
-from mujoco_py.modder import TextureModder
 
 from uitb.utils.logger import StateLogger, ActionLogger
 from uitb.utils.functions import output_path
+from uitb.simulator import Simulator
 
 
 def natural_sort(l):
@@ -21,95 +21,42 @@ def natural_sort(l):
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
     return sorted(l, key=alphanum_key)
 
-def grab_pip_image(env, modder):
+def grab_pip_image(env):
   # Grab an image from both 'for_testing' camera and 'oculomotor' camera, and display them 'picture-in-picture'
 
-  set_publication_mode(env, modder, True, skybox=True)
-
-  # Define image size
-  width, height = env.metadata["imagesize"]
-
-  if hasattr(env, 'target_plane_geom_idx'):
-    # Visualise target plane
-    env.model.geom_rgba[env.target_plane_geom_idx][-1] = 0.2
-
   # Grab images
-  img = np.flipud(env.sim.render(height=height, width=width, camera_name='for_testing'))
-  ocular_img = np.flipud(env.sim.render(height=env.ocular_image_height, width=env.ocular_image_width,
-                                        camera_name='oculomotor'))
+  img = env.viewer.render()
 
-  if hasattr(env, 'target_plane_geom_idx'):
-    # Disable target plane
-    env.model.geom_rgba[env.target_plane_geom_idx][-1] = 0.0
+  ocular_img = None
+  for module in env.perception.perception_modules:
+    if module.modality == "vision":
+      # TODO would be better to have a class function that returns "human-viewable" rendering of the observation;
+      #  e.g. in case the vision model has two cameras, or returns a combination of rgb + depth images etc.
+      ocular_img, _ = module.render()
 
-  # Resample
-  resample_factor = 3
-  resample_height = env.ocular_image_height*resample_factor
-  resample_width = env.ocular_image_width*resample_factor
-  resampled_img = np.zeros((resample_height, resample_width, 3), dtype=np.uint8)
-  for channel in range(3):
-    resampled_img[:, :, channel] = scipy.ndimage.zoom(ocular_img[:, :, channel], resample_factor, order=0)
+  if ocular_img is not None:
 
-  # Embed ocular image into free image
-  i = height - resample_height
-  j = width - resample_width
-  img[i:, j:] = resampled_img
+    # Resample
+    resample_factor = 2
+    resample_height = ocular_img.shape[0]*resample_factor
+    resample_width = ocular_img.shape[1]*resample_factor
+    resampled_img = np.zeros((resample_height, resample_width, 3), dtype=np.uint8)
+    for channel in range(3):
+      resampled_img[:, :, channel] = scipy.ndimage.zoom(ocular_img[:, :, channel], resample_factor, order=0)
 
-  set_publication_mode(env, modder, False, skybox=True)
+    # Embed ocular image into free image
+    i = env.viewer.resolution[1] - resample_height
+    j = env.viewer.resolution[0] - resample_width
+    img[i:, j:] = resampled_img
 
   return img
 
-def set_publication_mode(env, modder, value, skybox=True):
-
-  # Do nothing for remote driving env
-  if env.spec._env_name == "mobl-arms-remote-driving":
-    return
-
-  if value:
-
-    # Visualise muscle activation
-    env.model.tendon_rgba[:, 0] = 0.3 + env.sim.data.ctrl * 0.7
-
-    # Change floor color
-    #env.model.geom_rgba[env.model._geom_name2id["floor"]][3] = 0
-    env.model.geom_rgba[env.model._geom_name2id["floor"]] = [1, 1, 1, 0.2]
-    env.model.geom_matid[env.model._geom_name2id["floor"]] = 1
-
-    if skybox:
-      # Set skybox to light blue
-      skybox = modder.get_texture('skybox')
-      rgb = np.zeros((skybox.height, skybox.width, 3))
-      rgb[:, :] = np.array([204, 230, 255])
-      modder.set_rgb('skybox', rgb)
-
-    # Activate additional light
-    #env.model.light_active[env.model._light_name2id["additional"]] = 1
-
-  if not value:
-
-    # Set back to default tendon color, might affect policy
-    env.model.tendon_rgba[:, 0] = 0.95
-
-    if skybox:
-      # Set skybox back to black
-      modder = TextureModder(env.sim)
-      skybox = modder.get_texture('skybox')
-      arr = np.zeros((skybox.height, skybox.width, 3))*0
-      modder.set_rgb('skybox', arr)
-
-    # Change floor color back
-    #env.model.geom_rgba[env.model._geom_name2id["floor"]][3] = 1
-    env.model.geom_matid[env.model._geom_name2id["floor"]] = 0
-    env.model.geom_rgba[env.model._geom_name2id["floor"]] = [0.8, 0.6, 0.4, 1.0]
-
-    # Disable additional light
-    #env.model.light_active[env.model._light_name2id["additional"]] = 0
 
 if __name__=="__main__":
 
   parser = argparse.ArgumentParser(description='Evaluate a policy.')
-  parser.add_argument('config_file', type=str,
-                      help='config file used for training the model')
+  parser.add_argument('run_folder', type=str,
+                      help='the simulation folder')
   parser.add_argument('--checkpoint', type=str, default=None,
                       help='filename of a specific checkpoint (default: None, latest checkpoint is used)')
   parser.add_argument('--num_episodes', type=int, default=10,
@@ -124,16 +71,19 @@ if __name__=="__main__":
                       help='output file for action log if logging is enabled (default: ./action_log)')
   args = parser.parse_args()
 
+  # Add run folder to python path if not there already
+  if args.run_folder not in sys.path:
+    sys.path.insert(0, args.run_folder)
 
-  # If config file is given load that
-  with open(args.config_file, 'rb') as file:
-    config = pickle.load(file)
+  # Load config file
+  config_file = os.path.join(args.run_folder, 'config.dill')
+  with open(config_file, 'rb') as file:
+    config = dill.load(file)
 
   # Define output directories
-  env_name = config["name"]
-  run_folder = Path(args.config_file).parent.absolute()
-  checkpoint_dir = os.path.join(run_folder, 'checkpoints')
-  evaluate_dir = os.path.join(output_path(), config["name"], config["name"])
+  #config["use_cloned_files"] = False
+  checkpoint_dir = os.path.join(args.run_folder, 'checkpoints')
+  evaluate_dir = os.path.join(args.run_folder, 'evaluate')
 
   # Make sure output dir exists
   os.makedirs(evaluate_dir, exist_ok=True)
@@ -149,23 +99,22 @@ if __name__=="__main__":
   print(f'Loading model: {os.path.join(checkpoint_dir, model_file)}')
   model = PPO.load(os.path.join(checkpoint_dir, model_file))
 
-  # Load env_kwargs
-  env_kwargs = config["env_kwargs"]
-
-  # Override kwargs
-  env_kwargs["action_sample_freq"] = 100
-  env_kwargs["freq_curriculum"] = lambda : 1.0
-  env_kwargs["evaluate"] = True
-  env_kwargs["target_radius_limit"] = np.array([0.05, 0.1])
-  #env_kwargs["target_halfsize_limit"] = np.array([0.3, 0.3])
+  # Load and override run parameters
+  run_params = config["simulation"]["run_parameters"]
+  run_params["action_sample_freq"] = 100
+  #run_params["freq_curriculum"] = lambda : 1.0
+  run_params["evaluate"] = True
+  #run_params["target_radius_limit"] = np.array([0.05, 0.1])
+  #run_params["target_halfsize_limit"] = np.array([0.3, 0.3])
 
   # Use deterministic actions?
   deterministic = False
 
-  print(f"env_kwargs are: {env_kwargs}")
+  print(f"run parameters are: {run_params}")
 
   # Initialise environment
-  env = gym.make(env_name, **env_kwargs)
+  #env = gym.make(env_name, **env_kwargs)
+  env = Simulator(args.run_folder, run_parameters=run_params)
 
   if args.logging:
 
@@ -190,9 +139,7 @@ if __name__=="__main__":
       state_logger.log(episode_idx, state)
 
     if args.record:
-      modder = TextureModder(env.sim)
-      #set_publication_mode(env, modder, True, skybox=True)
-      imgs.append(grab_pip_image(env, modder))
+      imgs.append(grab_pip_image(env))
 
     # Loop until episode ends
     while not done:
@@ -203,7 +150,6 @@ if __name__=="__main__":
       # Take a step
       obs, r, done, info = env.step(action)
       reward += r
-      print(env.steps)
 
       if args.logging:
         action_logger.log(episode_idx, {"step": state["step"], "timestep": state["timestep"], "action": action.copy(),
@@ -213,16 +159,16 @@ if __name__=="__main__":
         state_logger.log(episode_idx, state)
 
       if args.record and not done:
-        imgs.append(grab_pip_image(env, modder))
+        imgs.append(grab_pip_image(env))
 
-    print(f"Episode {episode_idx}: {env.get_episode_statistics_str()}")
+    #print(f"Episode {episode_idx}: {env.get_episode_statistics_str()}")
 
-    episode_statistics = env.get_episode_statistics()
-    for key in episode_statistics:
-      statistics[key].append(episode_statistics[key])
+    #episode_statistics = env.get_episode_statistics()
+    #for key in episode_statistics:
+    #  statistics[key].append(episode_statistics[key])
 
-  print(f'Averages over {args.num_episodes} episodes (std in parenthesis):',
-        ', '.join(['{}: {:.2f} ({:.2f})'.format(k, np.mean(v), np.std(v)) for k, v in statistics.items()]))
+  #print(f'Averages over {args.num_episodes} episodes (std in parenthesis):',
+  #      ', '.join(['{}: {:.2f} ({:.2f})'.format(k, np.mean(v), np.std(v)) for k, v in statistics.items()]))
 
   if args.logging:
     # Output log
@@ -233,5 +179,5 @@ if __name__=="__main__":
 
   if args.record:
     # Write the video
-    env.write_video(imgs, os.path.join(evaluate_dir, args.out_file))
+    env.viewer.write_video(imgs, os.path.join(evaluate_dir, args.out_file))
     print(f'A recording has been saved to file {os.path.join(evaluate_dir, args.out_file)}')
