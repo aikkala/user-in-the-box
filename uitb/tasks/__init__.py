8f8eccb11f2a5b098a58a35bfa5d78ca440c38a0
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2022-05-18 08:55:42 +0300

    Add a choice reaction task

--- uitb/tasks/__init__.py
+++ uitb/tasks/__init__.py
@@ -1,3 +1,4 @@
 from .remote_driving.RemoteDriving import RemoteDriving
 from .pointing.Pointing import Pointing
 from .tracking.Tracking import Tracking
+from .choice_reaction.ChoiceReaction import ChoiceReaction
