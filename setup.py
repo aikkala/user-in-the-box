4ebbfe657e153efd926a26a232d7b49e45d52bad
Florian Fischer <62909493+fl0fischer@users.noreply.github.com>
2022-11-02 19:46:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='UIB',
       author_email="aleksi.ikkala@aalto.fi",
       packages=["UIB"],
       # this branch should be used for evaluation only
-      install_requires=['matplotlib', 'pandas', 'scipy', 'numpy', 'xmltodict',
+      install_requires=['gym', 'matplotlib', 'pandas', 'scipy', 'numpy', 'xmltodict',
                         'uitb_tools @ git+https://github.com/fl0fischer/uitb-tools.git#egg=uitb_tools-0.1.0'],
-      extra_requires={'training': ['gym', 'mujoco_py', 'stable-baselines3', 'tensorboard', 'torch']}
+      extra_requires={'training': ['mujoco_py', 'stable-baselines3', 'tensorboard', 'torch']}
 )
