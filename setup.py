05a304ac1dcbb57cc0a8d95788e48ba14c76404b
fl0fischer <florian.fischer26@gmail.com>
2022-11-14 18:03:52 +0100

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), "uitb/utils/__simulatorsdir__.
 
 setup(
    name='uitb',
-   version='0.2.0',
+   version='0.2.1',
    author='Aleksi Ikkala',
    author_email='aleksi.ikkala@gmail.com',
    packages=['uitb'],
