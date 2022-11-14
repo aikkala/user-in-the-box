90515cc56ece4e0a68a9442c26b6ed424a3bc2df
Florian Fischer <florian.j.fischer@uni-bayreuth.de>
2022-11-14 17:18:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 
 # Store simulators path before installing
 with open(os.path.join(os.path.dirname(__file__), "uitb/utils/__simulatorsdir__.py"), "w") as f:
+    f.write("# This file is read-only and should not be modified manually!\n"
+            "# To use a different output directory, set the 'simulator_folder' tag in the config file.\n\n")
     f.write("SIMULATORS_DIR = " + repr(os.path.normpath(os.path.abspath('simulators'))))
 
 setup(
