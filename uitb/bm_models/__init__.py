87b702bab4caea5200502a4e7d2ff25bf63d2dd0
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2023-03-24 19:06:41 +0200

    Fix incorrect import

--- uitb/bm_models/__init__.py
+++ uitb/bm_models/__init__.py
@@ -1,3 +1,3 @@
 from .mobl_arms_index.MoblArmsIndex import MoblArmsIndex
 from .mobl_arms.MoblArms import MoblArms
-from .mobl_arms.MoblArmsWrist import MoblArmsWrist
+from .mobl_arms_wrist.MoblArmsWrist import MoblArmsWrist
