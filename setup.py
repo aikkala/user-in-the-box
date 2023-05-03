726d9f7c9c1b5671bf187c1293f7f02ab97b4a22
Florian Fischer <florian.fischer26@gmail.com>
2023-05-03 20:30:48 +0200

    dependencies fixed

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
        "pygame",
        "pynput",
        "mujoco>=2.2.0",
+       "imageio",
        #"stable_baselines3>=1.4.0", 
        #"stable_baselines3 @ git+https://github.com/carlosluis/stable-baselines3.git@fix_tests#egg=stable_baselines3-2.0.0a0",
        "stable_baselines3>=2.0.0a1",
