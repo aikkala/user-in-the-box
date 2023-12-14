25f5c9b480bef2cd16d4b5e2d9de20a3a92417bb
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2023-12-14 15:58:55 +0200

    Add a bimanual mobl arms model

--- uitb/bm_models/__init__.py
+++ uitb/bm_models/__init__.py
@@ -1,3 +1,4 @@
 from .mobl_arms_index.MoblArmsIndex import MoblArmsIndex
 from .mobl_arms.MoblArms import MoblArms
 from .mobl_arms_wrist.MoblArmsWrist import MoblArmsWrist
+from .mobl_arms_bimanual_motor.MoblArmsBimanualMotor import MoblArmsBimanualMotor
